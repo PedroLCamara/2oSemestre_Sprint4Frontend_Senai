ecbe8c88febdc00df0537e113762ce8a5c04e917
PedroLCamara <plcrlschool@gmail.com>
2021-10-24 19:00:06 -0300

    GufiComunicacaoApi

--- gufi/senai-gufi-ui/src/pages/TiposEventos/TiposEventos.js
+++ gufi/senai-gufi-ui/src/pages/TiposEventos/TiposEventos.js
@@ -7,15 +7,46 @@ class TiposEventos extends Component{
     constructor(props){
         super(props);
         this.state = {
-            ListaTiposEventos: [{IdTipoEvento: 1, Titulo: 'a'}, {IdTipoEvento: 2, Titulo: 'b'}],
-            Titulo: '',
+            ListaTiposEventos: [],
+            idTipoEvento: 0,
+            tituloTipoEvento: '',
 
         };
 
     };
-    //O componente já foi inserido no DOM?
+
+    BuscarTiposEventos = () => {
+        console.log("HEY");
+        fetch('http://localhost:5000/api/TiposEventos') //endereço da api que irá se comunicar
+        .then( Resposta => Resposta.json()) //Deixa o formato do retorno da api como json
+        .then(dados => this.setState({ ListaTiposEventos: dados})) //Atualiza os dados da lista com o retorno da api
+        .catch(erro => console.log(erro))
+    }
+
+    AtualizaEstadoDoTitulo = async (event) => {
+        await this.setState({tituloTipoEvento: event.target.value})
+        console.log(event.target.value)
+    }
+
+    CadastrarTipoEvento = (event) => {
+        event.preventDefault();
+
+        fetch('http://localhost:5000/api/TiposEventos', { //endereço da api que irá se comunicar
+            
+            method: 'POST', //define o verbo da requisição
+            body: JSON.stringify({ //muda o objeto para JSON
+                tituloTipoEvento: this.state.tituloTipoEvento
+            }),
+            headers:{ "Content-Type" :"application/json"} //define que a comunicação será feita por JSON
+        })
+        .then(console.log("Cadastrado")) 
+        .then(this.BuscarTiposEventos) 
+        .catch(erro => console.log(erro))
+        .then(this.setState({tituloTipoEvento: ''}));
+    }
+
     componentDidMount(){
-    
+        this.BuscarTiposEventos()
     };
 
     //Renderizar
@@ -37,9 +68,9 @@ class TiposEventos extends Component{
                             <tbody>
                                 {
                                     this.state.ListaTiposEventos.map( (TipoEvento) => {
-                                        return( <tr key={TipoEvento.IdTipoEvento}> 
-                                                    <td>{TipoEvento.IdTipoEvento}</td> 
-                                                    <td>{TipoEvento.Titulo}</td> 
+                                        return( <tr key={TipoEvento.idTipoEvento}> 
+                                                    <td>{TipoEvento.idTipoEvento}</td> 
+                                                    <td>{TipoEvento.tituloTipoEvento}</td> 
                                                 </tr>)})
                                 }
                             </tbody>
@@ -48,7 +79,13 @@ class TiposEventos extends Component{
 
                     {/* Cadastro de tipos de eventos */}
                     <section>
-
+                        <h2>Cadastro de tipo de evento</h2>
+                        <form onSubmit={this.CadastrarTipoEvento}>
+                            <div>
+                                <input type="text" value={this.state.tituloTipoEvento} placeholder="Titulo do tipo de evento" onChange={this.AtualizaEstadoDoTitulo}></input>
+                                <button type="submit">Cadastrar</button>
+                            </div>
+                        </form>
                     </section>
                 </main>
             </div>
